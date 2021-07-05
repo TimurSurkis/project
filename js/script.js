5fd9134cdb7a99baed74669e971fc25bb1ef9b5e
Timur <example@gmail.com>
2021-07-05 13:41:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log('arr' + ' - object');
+
+let incr = 10, 
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
