06d8114e66cc8f25c2b93a29d791aa66c01f7935
TimurSurkis <timur.surkis@gmail.com>
2021-07-08 13:45:02 +0300

    task2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 "use strict";
 
-let num = 50;
+const numberOfMovies = +prompt("Сколько фильмов вы уже просмотрели?", "");
+const personalMovieDB = {
+    count: numberOfMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
-
-for (let i = 1; i < 10; i++) {
-    if(i === 6) {
-        // break;
-        continue;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько оцените его?');
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Продолжайте в том же духе');
+        }
+        else {
+            console.log('Error');
+            i--;
     }
-
-    console.log(i);
 }
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено маловато фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+            alert('Вы - киноман!');
+}
+
+console.log(personalMovieDB);
