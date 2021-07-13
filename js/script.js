6a9f506e1a0f6afd4c77cc7a63679580b13bd343
TimurSurkis <timur.surkis@gmail.com>
2021-07-13 11:53:29 +0300

    Объекты, деструктизация объектов

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 "use strict";
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    callback();
-    console.log(`Я учу: ${lang}!`);
-}
-
-function done() {
-    console.log('Какой язык программирования ты учишь?');
+const options = {
+    name: 'test',
+    width: 1024, 
+    height: 1024, 
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }, 
+    makeTest: function() {
+        console.log('Test!');
+    }
 };
 
-learnJS('JavaScript', done);
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options['colors']['border']);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
